f5834679a78940cbcc4b064ca5893ab168f6739c
Elena <lena.kostyuk@gmail.com>
2022-09-08 23:27:56 +0300

    code ver.1

--- task_C#_code/Program.cs
+++ task_C#_code/Program.cs
@@ -1,3 +1,28 @@
 ﻿// написать программу, которая из имеющегося массива строк формирует массив, длина строк которого меньше или равна трем
 
+string[] array1 = new string[] {"=)", "geek", "brains", "321", "SPb", "St Pete", "gk", "brn"};
+string[] array2 = new string[array1.Length];
 
+void SampleArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if(array1[i].Length <= 3)
+        {
+             array2[count] = array1[i];
+             count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array1.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SampleArray(array1, array2);
+PrintArray(array2);
